36041fbb20d2a0774c48121ea212d2e6da56e6e2
Anush Pallaev <77150208+Nushko@users.noreply.github.com>
2021-02-12 16:47:00 +0300

    Add files via upload

---
+++ lesson11_11.02.21/lesson11_11.02.21/DoMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson11_11._02._21
+{
+    delegate T ShowMath<T>(T x, T y);
+
+    public static class DoMath
+    {
+        public static T Add<T>(T x, T y)
+        {
+            return (dynamic)x + (dynamic)y;
+        }
+        public static T Substract<T>(T x, T y)
+        {
+            return (dynamic)x - (dynamic)y;
+        }
+        public static T Multiply<T>(T x, T y)
+        {
+            return Math.Round((dynamic)x * (dynamic)y, 2);
+        }
+        public static T Divide<T>(T x, T y)
+        {
+            return Math.Round((dynamic)x / (dynamic)y, 2);
+        }
+
+    }
+}
---
+++ lesson11_11.02.21/lesson11_11.02.21/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace lesson11_11._02._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, result;
+            ShowMath<double> show;
+
+            Console.WriteLine("Enter your numbers");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Choose your operation(+,-,/,*): ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            char cho = char.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            switch (cho)
+            {
+                case '+':
+                    show = DoMath.Add;
+                    result = show.Invoke(x, y);
+                    Color.Change(ConsoleColor.Green);
+                    Console.WriteLine($"{x} + {y} = {result}");
+
+                    break;
+                case '-':
+                    show = DoMath.Substract;
+                    result = show.Invoke(x, y);
+                    Color.Change(ConsoleColor.Green);
+                    Console.WriteLine($"{x} - {y} = {result}");
+                    Color.Change(ConsoleColor.White);
+                    break;
+                case '*':
+                    show = DoMath.Multiply;
+                    result = show.Invoke(x, y);
+                    Color.Change(ConsoleColor.Green);
+                    Console.WriteLine($"{x} * {y} = {result}");
+                    Color.Change(ConsoleColor.White);
+                    break;
+                case '/':
+                    show = DoMath.Divide;
+                    result = show.Invoke(x, y);
+                    Color.Change(ConsoleColor.Green);
+                    Console.WriteLine($"{x} / {y} = {result}");
+                    Color.Change(ConsoleColor.White);
+                    break;
+                default:
+                    Color.Change(ConsoleColor.Red);
+                    Console.WriteLine("Wrong operation!");
+                    Color.Change(ConsoleColor.White);
+                    break;
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+    class Color
+    {
+        public static void Change(ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+        }
+    }
+}
